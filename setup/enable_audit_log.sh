9d454f1d145cd7f0cef53dc534d9fdf65379ccff
lionelstellar <1023612737@qq.com>
2021-12-15 11:51:34 +0800

    update

--- setup/enable_audit_log.sh
+++ setup/enable_audit_log.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+sudo cp $K8S_SCRIPTS_DIR/host_configs/audit.yaml  /etc/kubernetes/pki/audit-policy.yaml
 
-cp $K8S_SCRIPTS_DIR/host_configs/audit.yaml  /etc/kubernetes/pki/audit-policy.yaml
-cp $K8S_SCRIPTS_DIR/host_configs/kube-apiserver-log.yaml  /etc/kubernetes/manifests/kube-apiserver.yaml
+id foyjog
+if [ $? == 0 ]; then
+    echo "on foyjog's server"
+    sudo cp $K8S_SCRIPTS_DIR/host_configs/kube-apiserver-log.yaml  /etc/kubernetes/manifests/kube-apiserver.yaml
+else
+    echo "not on foyjog's server"
+    sudo cp $K8S_SCRIPTS_DIR/host_configs/kube-apiserver-log-foyjog.yaml  /etc/kubernetes/manifests/kube-apiserver.yaml
+fi
+
+
+
 
--- setup/enable_audit_webhook.sh
+++ setup/enable_audit_webhook.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-cp $K8S_SCRIPTS_DIR/host_configs/audit.yaml  /etc/kubernetes/pki/audit-policy.yaml
-cp $K8S_SCRIPTS_DIR/host_configs/audit-webhook.yaml /etc/kubernetes/pki/audit-webhook.yaml
-cp $K8S_SCRIPTS_DIR/host_configs/kube-apiserver-webhook.yaml  /etc/kubernetes/manifests/kube-apiserver.yaml
+sudo cp $K8S_SCRIPTS_DIR/host_configs/audit.yaml  /etc/kubernetes/pki/audit-policy.yaml
 
+id foyjog
+if [ $? == 0 ]; then
+    echo "on foyjog's server"
+    sudo cp $K8S_SCRIPTS_DIR/host_configs/audit-webhook-foyjog.yaml /etc/kubernetes/pki/audit-webhook.yaml
+    sudo cp $K8S_SCRIPTS_DIR/host_configs/kube-apiserver-webhook-foyjog.yaml  /etc/kubernetes/manifests/kube-apiserver.yaml
+
+else
+    echo "not on foyjog's server"
+    sudo cp $K8S_SCRIPTS_DIR/host_configs/audit-webhook.yaml /etc/kubernetes/pki/audit-webhook.yaml
+    sudo cp $K8S_SCRIPTS_DIR/host_configs/kube-apiserver-webhook.yaml  /etc/kubernetes/manifests/kube-apiserver.yaml
+
+fi
