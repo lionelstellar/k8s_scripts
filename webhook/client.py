fd60133313be2643c06956cf6e48be0838a523d2
lionelstellar <1023612737@qq.com>
2021-12-15 11:55:20 +0800

    update

--- webhook/client.py
+++ webhook/client.py
@@ -3,6 +3,7 @@ import requests
 
 payload = '{"hello": "world"}'
 url = "http://127.0.0.1:8080"
+# url = "http://10.78.119.237:8080"
 r = requests.post(url, data=payload)
 print(r.status_code)
 print(r.content.decode("UTF-8"))
