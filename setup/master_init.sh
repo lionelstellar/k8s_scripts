2fbf903d6d085b901c8a9ef1db8b90d801428ab5
lionelstellar <1023612737@qq.com>
2021-12-16 10:52:33 +0800

    update

--- setup/master_init.sh
+++ setup/master_init.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+id foyjog
+if [ $? == 0 ]; then
+    echo "on foyjog's server"
+    CONFIG=foyjog_config
+else
+    echo "not on foyjog's server"
+    CONFIG=config
+fi
+
 while read line;do  
     eval "$line"  
-done < $K8S_SCRIPTS_DIR/config/config
+done < $K8S_SCRIPTS_DIR/config/$CONFIG
 
 # 根据实际情况修改, 默认唯一master
 master_ip=${MASTER_IP_LIST[0]}
